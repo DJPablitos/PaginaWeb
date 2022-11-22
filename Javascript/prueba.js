d7c4160e0129b36711369cf51d85f2910368c23b
DJPablitos <guerra.pablo.daniel@gmail.com>
2022-11-22 12:30:08 -0300

    Cuarto

---
+++ Javascript/prueba.js
@@ -0,0 +1,12 @@
+console.log("Trabajamos con javascript y node.js")
+console.log("Trabajamos con javascript y node.js")
+console.log("Trabajamos con javascript y node.js")
+console.log("Trabajamos con javascript y node.js")
+// condicionales
+
+if (true) {
+    console.log("Es true y ejecuto los codigos de las llaves")
+}
+if (false) {
+    console.log("Es false y no ejecuto los codigos de las llaves")
+}
